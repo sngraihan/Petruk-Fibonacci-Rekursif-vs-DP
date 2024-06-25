774f3f00669a5ae86ada81a850336f4d741e7d3d
sngraihan <raihan.sng123@gmail.com>
2024-06-25 12:22:28 +0700

    rekursif vs dynamicc programming

---
+++ tugas petruk fibonacci.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <iomanip>
+using namespace std;
+using namespace chrono;
+
+unsigned long long fibonacciRekursif(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    return fibonacciRekursif(n - 1) + fibonacciRekursif(n - 2);
+}
+
+unsigned long long fibonacciDP(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    vector<unsigned long long> fib(n + 1);
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
+}
+
+template<typename Func>
+double time(Func func, int n) {
+    auto start = high_resolution_clock::now();
+    func(n);
+    auto end = high_resolution_clock::now();
+    duration<double, milli> duration = end - start;
+    return duration.count();
+}
+
+
+
+int main() {
+    int n_values[] = {10, 25, 30, 40, 50};
+    int num_iterations = 1000;
+
+    for (int n : n_values) {
+        unsigned long long result_rekursif = fibonacciRekursif(n);
+        unsigned long long result_dp = fibonacciDP(n);
+
+		double rtime = time(fibonacciRekursif, n);
+		double dptime = time(fibonacciDP, n);
+		
+		
+        cout << "n = " << n << endl;
+        cout << "Rekursif: " << result_rekursif << ", waktu: " << fixed<<setprecision(50) <<rtime << " miliseconds" << endl;
+        cout << "Dynamic Programming: " << result_dp << ", waktu rata-rata: " <<fixed<< setprecision(50) << dptime <<" miliseconds" << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
+
